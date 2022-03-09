b70b0a0f029035dd777a8005ad248d25f7d0f97e
Reshetov Denis <reshetovdenis@gmail.com>
2022-03-09 14:04:19 +0300

    Added QR code and text hashing.

--- src/CreateLetter.js
+++ src/CreateLetter.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react'
-import { Form, Input, TextArea, Grid } from 'semantic-ui-react'
-import { TxButton } from './substrate-lib/components'
+import { Form, Input, TextArea, Grid, Button, Label } from 'semantic-ui-react'
 import { useSubstrateState } from './substrate-lib'
+import QRCode from 'qrcode.react';
+import { sha256 } from 'js-sha256';
 
 export default function Main(props) {
   const [status, setStatus] = useState(null)
-  const [formState, setFormState] = useState({ addressTo: '', amount: 0 })
+  const [formState, setFormState] = useState({ letterInfo: '', text: '', addressTo: '', amount: 0 })
 
   const onChange = (_, data) =>
     setFormState(prev => ({ ...prev, [data.state]: data.value }))
 
-  const { addressTo, amount } = formState
+  const { letterInfo, text, addressTo, amount } = formState
 
   const { keyring } = useSubstrateState()
   const accounts = keyring.getPairs()
@@ -24,6 +25,24 @@ export default function Main(props) {
     })
   })
 
+  const getLetterInfo = () => {
+    // skill_ipfs_hash | insurance_id | teach_address | stud_address | amount | teach_sign_1 | teach_sign_2
+
+    return sha256(text) + getLetterId() + addressTo + amount;
+  }
+
+  const getLetterId = () => {
+    const usedId = parseInt(window.localStorage.getItem('letterId')) || 0;
+    const letterId = 1+usedId;
+    window.localStorage.setItem('letterId', letterId);
+    return letterId;
+  }
+
+  const showQR = () => {
+    const data = getLetterInfo();
+    setFormState({ ...formState, letterInfo: data });
+  }
+
   return (
     <Grid.Column width={8}>
       <h1>Create recommendation letter</h1>
@@ -33,8 +52,8 @@ export default function Main(props) {
             fluid
             type="text"
             placeholder="Recommendation letter text"
-            value={addressTo}
-            state="addressTo"
+            value={text}
+            state="text"
             onChange={onChange}
           />
         </Form.Field>
@@ -59,18 +78,22 @@ export default function Main(props) {
           />
         </Form.Field>
         <Form.Field style={{ textAlign: 'center' }}>
-          <TxButton
-            label="Create"
-            type="SIGNED-TX"
+          <Button
             setStatus={setStatus}
-            attrs={{
-              palletRpc: 'balances',
-              callable: 'transfer',
-              inputParams: [addressTo, amount],
-              paramFields: [true, true],
+            onClick={()=>{
+              showQR();
             }}
-          />
+          >Create</Button>
         </Form.Field>
+
+        <Form.Field>
+          <Label basic color="teal">
+            {letterInfo}
+          </Label>
+          <br/>
+          <QRCode value={letterInfo} />
+        </Form.Field>
+
         <div style={{ overflowWrap: 'break-word' }}>{status}</div>
       </Form>
     </Grid.Column>
