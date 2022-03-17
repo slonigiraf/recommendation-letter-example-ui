b28c9fa022d97233ce49b658fdcea29e4d125af3
Denis Reshetov <reshetovdenis@gmail.com>
2022-03-17 11:25:33 +0300

    Fixed TxButton

--- src/UseInsurance.js
+++ src/UseInsurance.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react'
-import { Form, Grid, Button, List, Label } from 'semantic-ui-react'
+import React, { useState, useEffect } from 'react'
+import { Form, Grid, List, Label } from 'semantic-ui-react'
 import { useSubstrateState } from './substrate-lib'
 import { web3FromSource } from '@polkadot/extension-dapp'
 import { TxButton } from './substrate-lib/components'
-import { ApiPromise } from '@polkadot/api'
 import { u8aToHex } from '@polkadot/util'
 
 export default function Main(props) {
@@ -14,7 +13,15 @@ export default function Main(props) {
 
   const { currentAccount } = useSubstrateState()
   const [status, setStatus] = useState(null)
-  const [employerAddress, setEmployerAddress] = useState('')
+
+  const [insurance_id, setInsurance_id] = useState('')
+  const [guaranteeHex, setGuaranteeHex] = useState('')
+  const [workerHex, setWorkerHex] = useState('')
+  const [employerHex, setEmployerHex] = useState('')
+  const [guaranteeSignatureHex, setGuaranteeSignatureHex] = useState('')
+  const [workerSignatureHex, setWorkerSignatureHex] = useState('')
+
+
   const { keyring } = useSubstrateState()
   const accounts = keyring.getPairs()
 
@@ -27,41 +34,16 @@ export default function Main(props) {
     })
   })
 
-  const enablePenalization = async () => {
+  useEffect(async () => {
     const [employer,] = await getFromAcct()
-    setEmployerAddress(employer.address)
-    //----
-    // Create a transaction
-    const api = await ApiPromise.create()
-    //[letterId, guaranteeAddress, workerAddress, employerAddress, amount, guaranteeSignOverReceipt, workerSignOverInsurance]
-    const insurance_id = letterId
-    const guaranteeHex = guaranteeAddress
-    const workerHex = workerAddress
-    const employerHex = u8aToHex(employer.publicKey)
-    const guaranteeSignatureHex = guaranteeSignOverReceipt
-    const workerSignatureHex = workerSignOverInsurance
 
-    // console.log("------------")
-    // console.log("letterId", letterId)
-    // console.log("guaranteeHex", guaranteeHex)
-    // console.log("workerHex", workerHex)
-    // console.log("employerHex", employerHex)
-    // console.log("amount", amount)
-    // console.log("guaranteeSignatureHex", guaranteeSignatureHex)
-    // console.log("workerSignatureHex", workerSignatureHex)
-    // console.log("------------")
-    
-    const reimburse = api.tx.insurances.reimburse(insurance_id,
-      guaranteeHex,
-      workerHex,
-      employerHex,
-      amount,
-      guaranteeSignatureHex,
-      workerSignatureHex)
-    // Sign and send the transaction using our account
-    const hash = await reimburse.signAndSend(employer)
-    console.log('Transfer sent with hash', hash.toHex())
-  }
+    setInsurance_id(letterId)
+    setGuaranteeHex(guaranteeAddress)
+    setWorkerHex(workerAddress)
+    setEmployerHex(u8aToHex(employer.publicKey))
+    setGuaranteeSignatureHex(guaranteeSignOverReceipt)
+    setWorkerSignatureHex(workerSignOverInsurance)
+  }, []);
 
   const getFromAcct = async () => {
     const {
@@ -82,39 +64,35 @@ export default function Main(props) {
   return (
     <Grid.Column width={8}>
       <List divided selection>
-          <List.Item>
-            <Label horizontal>Text</Label>
-            {props.text}
-          </List.Item>
-          <List.Item>
+        <List.Item>
+          <Label horizontal>Text</Label>
+          {props.text}
+        </List.Item>
+        <List.Item>
           <Label as='a' tag>{workerAddress}</Label>
-          </List.Item>
-        </List>
+        </List.Item>
+      </List>
       <Form>
 
         <Form.Field style={{ textAlign: 'center' }}>
 
           <TxButton
-            label="Not working"
+            label="Get a reimbursement"
             type="SIGNED-TX"
             setStatus={setStatus}
-            txOnClickHandler={() => {
-              enablePenalization()
-            }}
             attrs={{
               palletRpc: 'insurances',
               callable: 'reimburse',
-              inputParams: [letterId, guaranteeAddress, workerAddress, employerAddress, amount, guaranteeSignOverReceipt, workerSignOverInsurance],
+              inputParams: [insurance_id,
+                guaranteeHex,
+                workerHex,
+                employerHex,
+                amount,
+                guaranteeSignatureHex,
+                workerSignatureHex],
               paramFields: [true, true, true, true, true, true, true],
             }}
           />
-
-          <Button
-            setStatus={setStatus}
-            onClick={() => {
-              enablePenalization();
-            }}
-          >Penalize</Button>
         </Form.Field>
         <div style={{ overflowWrap: 'break-word' }}>{status}</div>
       </Form>
